8cee96d1157532589d91f480ff51f1c58d3ec6a0
BLOKEPICK <Blokeplaycom@gmail.com>
2025-09-01 23:45:28 -0400

    UP

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,17 @@
+import type { APIRoute } from 'astro';
+
+const SITE = 'https://dessencewellness.com';
+
+const routes = ['/', '/services', '/about', '/contact', ['specialty-treatments', 'waxing', 'facials', 'spider-vein-treatment', 'vitamin-injections', 'prp-platelet-rich-plasma', 'nefertiti-neck', 'neurotoxins', 'skinvive', 'skincare', 'teeth-whitening', 'filler-dissolving', 'juvederm-fillers', 'smooth-pdo-threads', 'microneedling', 'aerolase', 'chemical-peels'].map(s => `/services/${s}`)].flat();
+
+export const GET: APIRoute = () => {
+  const urls = routes.map((path) => `  <url><loc>${SITE}${path}</loc><changefreq>weekly</changefreq><priority>${path==='/'? '1.0':'0.7'}</priority></url>`).join('\n');
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+</urlset>`;
+  return new Response(xml, {
+    status: 200,
+    headers: { 'Content-Type': 'application/xml; charset=UTF-8' }
+  });
+};
