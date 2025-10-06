b8ad5a6d2bfe1389417da5e5b02b8844ff950a17
BLOKEPICK <Blokeplaycom@gmail.com>
2025-10-06 11:19:34 -0400

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  build: { format: 'file' },
+  trailingSlash: 'always', // genera /about-us/index.html, /our-approach/index.html, etc.
   alias: {
     '@': '/src',
   },
