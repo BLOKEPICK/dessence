8b8dab1529d3aa45ba76d968c9cc71f1f86d6ebf
BLOKEPICK <Blokeplaycom@gmail.com>
2025-11-22 02:36:35 -0500

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
+  site: 'https://www.d-essencewellness.com',
   output: 'static',
   trailingSlash: 'always',
-  alias: { '@': '/src' }
+  alias: { '@': '/src' },
+  integrations: [
+    sitemap()
+  ]
 });
